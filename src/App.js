ef5c16a2c67f6ee1f1e199f59b38d11cf5e03986
MoeBoukhriss <mohammed.boukhriss@outlook.com>
2020-02-19 01:33:31 -0500

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -29,12 +29,8 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          },
-          () => {
-            console.log(this.state);
-          })
-        });
-      } else {
+          });
+      })} else {
         this.setState({currentUser: userAuth});
       }
     });
